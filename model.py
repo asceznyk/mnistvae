89093206a0741c332f7f2bcecd64673d37aaed4f
asceznyk <asceznyk@gmail.com>
2022-04-10 19:06:51 +0530

    .. idk wtf this is

--- model.py
+++ model.py
@@ -6,23 +6,24 @@ import torch.nn.functional as F
 
 class Encoder(nn.Module):
     def __init__(self, img_dim, z_dim):
-        super(Encoder,self).__init__()
+        super(Encoder, self).__init__()
         self.img_dim = img_dim
         self.z_dim = z_dim
 
         self.enc = nn.Sequential(
-            nn.Conv2d(img_dim[0], 32, kernel_size=3, stride=2),
+            nn.Conv2d(img_dim[0], 32, kernel_size=3, stride=2, pad=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, pad=1),
             nn.ReLU(),
         )
 
-        self.dense = nn.Linear(64*6*6, 16)
+        self.dense = nn.Linear(64*7*7, 16)
         self.mu = nn.Linear(16, z_dim)
         self.sigma = nn.Linear(16, z_dim)
 
     def forward(self, x):
         x = self.enc(x)
+        print(x.size())
         x = self.dense(x.view(x.size()[0], -1))
         z_mean = self.mu(x)
         z_std = self.sigma(x)
@@ -30,10 +31,21 @@ class Encoder(nn.Module):
         z = z_mean + torch.exp(z_std * 0.5) * eps
         return z, z_mean, z_std
 
+class Decoder(nn.Module):
+    def __init__(self, img_dim, z_dim):
+        super(Decoder, self).__init__()
+        self.img_dim = img_dim
+        self.z_dim = z_dim
 
+        self.dense = nn.Linear(z_dim, 6*6*64)
 
-
-
+        self.dec = nn.Sequential(
+            nn.ConvTranspose2d(64, 64, kernel_size=3, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2)
+            nn.ReLU(),
+            nn.ConvTranspose2d()
+        )
 
 
 
