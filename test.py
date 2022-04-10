211c11679b3534e1b761d37986485dc1aa397a46
asceznyk <asceznyk@gmail.com>
2022-04-10 17:28:58 +0530

    .. idk wtf this is

--- test.py
+++ test.py
@@ -3,20 +3,26 @@ import pandas as pd
 
 import torch
 
+from torch.utils.data import DataLoader
+
 import torchvision
 from torchvision.datasets import MNIST
 
-def pil_to_tensor(img):
-    print(torch.from_numpy(np.array(img)).unsqueeze(0).size())
-    return torch.from_numpy(np.array(img))
+def pil_to_tensor(imgs):
+    return torch.from_numpy(np.array(imgs)).unsqueeze(0)
     
 def main():
     latent_dim = 2
     mnist_data = MNIST(root='./', download=True)
 
-    img, _ = mnist_data[0]
+    loader = DataLoader(mnist_data, 
+                        batch_size=8, 
+                        shuffle=False, 
+                        collate_fn=pil_to_tensor)
 
-    pil_to_tensor(img)
+    bt = next(iter(loader))
+
+    print(bt.size)
 
 if __name__ == '__main__':
     main()
