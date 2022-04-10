308fec389d7bfcb0616b2dbb74054161060216fb
asceznyk <asceznyk@gmail.com>
2022-04-10 16:53:40 +0530

    .. idk wtf this is

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pandas as pd
 import torchvision
 from torchvision.datasets import MNIST
 
-mnist_data = MNIST(root='./')
+mnist_data = MNIST(root='./', download=True)
 
 print(mnist_data[0])
 
