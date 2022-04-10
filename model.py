b2a97aa3b5ec6a8c03a55107562c396e7977c1e1
asceznyk <asceznyk@gmail.com>
2022-04-10 18:27:34 +0530

    .. idk wtf this is

--- model.py
+++ model.py
@@ -11,20 +11,20 @@ class Encoder(nn.Module):
         self.z_dim = z_dim
 
         self.enc = nn.Sequential(
-            nn.Conv2d(img_dim[0], 32, kernel_size=3, stride=2, padding='same'),
+            nn.Conv2d(img_dim[0], 32, kernel_size=3, stride=2),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding='same'),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2),
             nn.ReLU(),
         )
 
-        self.dense = nn.Linear(64*7*7, 16)
+        self.dense = nn.Linear(64*6*6, 16)
         self.mu = nn.Linear(16, z_dim)
         self.sigma = nn.Linear(16, z_dim)
 
     def forward(self, x):
         x = self.enc(x)
-        print(x.size())
-        x = self.dense(x.view(-1))
+        print(x.view(0,-1).size())
+        x = self.dense(x.view(0, -1))
         z_mean = self.mu(x)
         z_std = self.sigma(x)
         z = torch.normal(z_mean, z_std)
